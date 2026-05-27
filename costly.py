1e39f3653ff7232b9c86243801e162043f7f2b46
Shenard Williams <smw140@duke.edu>
2026-05-26 22:57:04 -0400

    1st Commit: Create of hands for user and computer as well as initial parts of computer strategy

---
+++ costly.py
@@ -0,0 +1,107 @@
+import random
+
+suits = ["Diamonds", "Hearts", "Clubs", "Spades", ]
+ranks = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
+
+a = [] #user's hand
+b = [] #computer's hand 
+a_points = int(0)
+b_points = int(0)
+
+def cards():
+    deck = []
+    for suit in suits:  #for every suit
+        for rank in ranks:   #going through every number assign the suit to this number 
+            card = rank + ' of ' + suit
+            deck.append(card)
+    return deck
+        
+def start(deck:list):
+    #shuffle the deck of cards
+    random.shuffle(deck)
+    random.shuffle(deck)
+    random.shuffle(deck)
+    index_a = random.randint(0, 51)  #find index for picking a cut in the deck
+    index_b = random.randint(0, 50)
+    a_deal = deck.pop(index_a)       #take out a cut 
+    b_deal = deck.pop(index_b)          
+    cuts = [a_deal, b_deal]          #save cut card strings to a list 
+    deck.append(a_deal)              #return cut cards to the deck 
+    deck.append(b_deal)      
+    for item in cuts:                #comapre who has the lower cut 
+        if item[0:3] == 'Jack' or "King" or "Quee":
+            item = int(10) 
+        elif item[0:2] == 'Ace':
+            item = int(1)
+        elif item[0:1] == '10':
+            item = int(10)
+        else: 
+            item = int(item[0])
+    if cuts[0] < cuts[1]:
+        a.append('*D*')
+        b.append('#')
+    else: 
+        b.append('*D*')
+        a.append('#')
+    random.shuffle(deck)
+    return deck
+
+def deal(deck:list): 
+    #"deal the cards" by popping the first item of the deck list
+    for i in range(6):
+        hand = deck.pop(0)
+    #put that item (card) into the alternating lists of a and b 
+        if b[0] == '*D*':  #if computer is dealer deal to user first
+            if i % 2 != 0:
+                b.append(hand)
+            else: 
+                a.append(hand)
+        elif b[0] == '#':   #if user is the dealer
+            if i % 2 != 0:
+                a.append(hand)
+            else: 
+                b.append(hand)
+    #make the deck card 
+    top = deck.pop(0) 
+    top_card = "Top card is: " + top 
+#print the list that a (user) has 
+    print(a)
+    print(top_card)
+    return top
+
+#Pegging play
+def play(top:str):
+    #if top card is Jack or Deuce add 4 points to dealer of 'His Heels'
+    top_type = top[0:3]
+    if top_type == 'Jack':
+        if a[0] == '*D*':
+            a_points += 4
+        else:
+            b_points += 4
+        print('His Heels +4')
+    if top_type == '2 of':
+        if a[0] == '*D*':
+            a_points += 4
+        else:
+            b_points += 4
+        print('Duece Down +4')
+    
+    #Pegging Play
+    #Non-dealer starts
+    #if a[0] == '*D*':
+        #flop = int(input("Choose card # 1, 2, or 3: ")) #user enters integer of card
+    #else: 
+        #random.choices(b[1:3], weights=[])
+        #xflop = b[0]
+        
+
+def main(): 
+    main_deck = cards()
+    start(main_deck)
+    trump = deal(main_deck)
+    play(trump)
+
+
+if __name__ == '__main__':
+    main()    
+
---
+++ strategy.py
@@ -0,0 +1,46 @@
+import random
+from costly import b
+#Computer Logic
+
+#Analyze Hand
+hand = b[1:3] 
+
+#Sequence (Prials)
+def sequence(hand):
+    #turn all cards in hand into their respective numerical order
+    for card in hand:                
+        if card[0:3] == 'Jack':
+            card = 11
+        if card[0:3] == 'Quee':
+            card = 12
+        if card[0:3] == 'King':
+            card = 13
+        if card[0:2] == 'Ace':
+            card = 1
+        else:
+            card == int(card[0])
+    ordered = sorted(hand)
+    #check to see if these cards are in a sequence
+    run_1 = hand[1] - 1
+    run_2 = hand[2] - 1
+    if (hand[0] == run_1) and (hand[1] == run_2):
+        return True
+    else:
+        return False 
+
+#Addition to 15, 25, or 31  
+def addition(hand):
+    #turn all cards in hand into their numerical values 
+    for card in hand:                
+        if card[0:3] == 'Jack' or "King" or "Quee":
+            card = int(10) 
+        elif card[0:2] == 'Ace':
+            card = int(1)
+        elif card[0:1] == '10':
+            card = int(10)
+        else: 
+            card = int(card[0])
+    
+
+#First Card: We 
+#for card in b[1:3]:
